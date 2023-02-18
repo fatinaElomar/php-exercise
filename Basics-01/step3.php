939efe472fb4ee8200ac0fc3f5dea45211c4cda3
fatinaelomar3 <fatinaelomar3@gmail.com>
2023-02-18 14:36:35 +0200

    Step 3 : Variables lvl 1

---
+++ Basics-01/step3.php
@@ -0,0 +1,11 @@
+<?php
+$name="fatina";
+$area="";
+$email="fatina@gmail.com";
+$height="156";
+$width="55";
+$area= $width+$height;
+echo("the area value is:".$area);
+
+echo 'My name is "' . $name . '" and my email is "' . $email . '".';
+?>
